b5bc0cf8a4b057a84670d7b3c098ddc4897eb46b
Amirmohammad Taghizadehgan <amirm.taghizadegan@gmail.com>
2023-10-23 09:07:20 +0330

    main frame

--- main.py
+++ main.py
@@ -9,34 +9,41 @@ class Main_Window(QtWidgets.QWidget):
         super().__init__()
         self.setGeometry(350, 150, 550, 350)
         self.setGeometry(pos.x(), pos.y(), size[0], size[1])
-        self.setWindowTitle("Main Menu")
+        self.setWindowTitle("Machine Vision")
         self.setWindowIcon(QtGui.QIcon("images/logo.jpeg"))
         self.UI()
         self.show()
 
 
     def mainDesign(self):
+        ## toolbox
         # buttons
-        self.openFile_button = QtWidgets.QPushButton("open file")
-        self.saveFile_button = QtWidgets.QPushButton("save file")
-        self.saveAsFile_button = QtWidgets.QPushButton("save file")
-        self.apply_button = QtWidgets.QPushButton("apply")
+        self.openFile_button = QtWidgets.QPushButton()
+        self.saveFile_button = QtWidgets.QPushButton()
+        self.saveAsFile_button = QtWidgets.QPushButton()
+        self.apply_button = QtWidgets.QPushButton()
+        
+        # drop down
+        self.filter_list = QtWidgets.QComboBox() 
+        self.filter_list.addItem("hello")
 
-        # Set icons for the buttons
-        self.openFile_button.setIcon(QtGui.QIcon("button1_icon.png"))
-        self.saveFile_button.setIcon(QtGui.QIcon("button2_icon.png"))
-        self.saveAsFile_button.setIcon(QtGui.QIcon("button3_icon.png"))
-        self.apply_button.setIcon(QtGui.QIcon("button4_icon.png"))
+        # toolbox icons
+        self.openFile_button.setIcon(QtGui.QIcon("images/open_file.png"))
+        self.saveFile_button.setIcon(QtGui.QIcon("images/save.png"))
+        self.saveAsFile_button.setIcon(QtGui.QIcon("images/save as.png"))
+        self.apply_button.setIcon(QtGui.QIcon("images/submit.png"))
 
         # 
 
     def layouts(self):
         # toolbox layout
         self.toolbox = QtWidgets.QHBoxLayout()
-        self.toolbox.addItem(self.openFile_button)
-        self.toolbox.addItem(self.saveFile_button)
-        self.toolbox.addItem(self.saveAsFile_button)
-        self.toolbox.addItem(self.apply_button)
+        self.toolbox.addWidget(self.openFile_button)
+        self.toolbox.addWidget(self.saveFile_button)
+        self.toolbox.addWidget(self.saveAsFile_button)
+        self.toolbox.addWidget(self.filter_list)
+        self.toolbox.addWidget(self.apply_button)
+        self.toolbox.addStretch()
 
         # hyper parameters layout
 
@@ -52,6 +59,7 @@ class Main_Window(QtWidgets.QWidget):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
+    app.setWindowIcon(QtGui.QIcon("./images/logo.jpeg"))
     window = Main_Window()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
